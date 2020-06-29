f52fd581a58958d677b15bf3dd317a7da97d6bb7
TongJorim <fuxjohe@gmail.com>
2020-06-29 19:52:13 +0900

    fullpage 레이아웃
    visual / about / skill / portfolio 으로  풀페이지 레이아웃

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+        scrollHorizontally: true,
+        licenseKey:"1B89B8BC-AFC2417F-BAAB0959-319022D1"
+	});
+});
